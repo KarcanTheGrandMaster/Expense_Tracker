4a4763c3e2938325c73cb2f8da3b67c269d1b24f
KarcanTheGrandMaster <karcankublay2000@gmail.com>
2024-03-09 20:49:06 +0300

    add a dark mode and remove color
    add a dark mode and remove color

--- tracker_app/lib/main.dart
+++ tracker_app/lib/main.dart
@@ -1,29 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:tracker_app/widgets/expenses_list/expenses.dart';
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125));
+
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(
+      darkTheme: ThemeData.dark().copyWith(
         useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 10,
+          ),
         ),
-
-        //cardTheme: const CardTheme().copyWith(color:kColorScheme.secondaryContainer,
-        //margin: const EdgeInsets.symmetric(horizontal: 32,vertical 9,))
-
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
           ),
         ),
       ),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              )),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 32,
+                ),
+              )),
+      //themeMode: ThemeMode.dark,
       home: const Expenses(),
     ),
   );
